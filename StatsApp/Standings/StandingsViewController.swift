fa752e5b47463b1d46762c0e5ac14f389ef60c67
davut <davuthankurt@gmail.com>
2024-10-13 20:33:19 +0300

    table view adjustments

--- StatsApp/Standings/StandingsViewController.swift
+++ StatsApp/Standings/StandingsViewController.swift
@@ -20,14 +20,24 @@ class StandingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.backgroundColor = .white
         let tableView = UITableView()
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        tableView.register(StandingsCell.self, forCellReuseIdentifier: "Cell")
         tableView.dataSource = self
         tableView.delegate = self
         
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         self.tableView = tableView
         view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        viewModel?.loadStandings()
     }
 }
 
@@ -45,14 +55,35 @@ extension StandingsViewController: StandingsViewModelDelegate {
 
 extension StandingsViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 1
+        guard let table = leagueTable?.standings.first else { return 0 }
+        return table.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = leagueTable?.standings.first?.first?.team.name
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? StandingsCell else { return UITableViewCell() }
+        guard let table = leagueTable?.standings.first else { return cell }
+        let team = table[indexPath.row]
+        
+        cell.rank.text = "\(team.rank)."
+        cell.team.text = team.team.name
+//        cell.form.text = team.form
+        cell.goalsDiff.text = "\(team.goalsDiff)"
+        cell.points.text = "\(team.points)"
+//        cell.records.text = "\(team.records.win)"
+    
         return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = StandingsCell()
+        headerView.frame = CGRect(x: 0, y: 0, width: tableView.frame.width, height: 50)
+        headerView.backgroundColor = .green
+
+        return headerView
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
     }
 }
 
