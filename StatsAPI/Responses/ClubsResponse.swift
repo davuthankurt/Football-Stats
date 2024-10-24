1497ed7a39407f2c821391b38f9d73e41219c113
davut <davuthankurt@gmail.com>
2024-10-24 20:11:53 +0300

    api response handled

--- StatsAPI/Responses/ClubsResponse.swift
+++ StatsAPI/Responses/ClubsResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public struct ClubsResponse: Codable {
+    private enum RootCodingKeys: String, CodingKey {
+        case response
+    }
+    
+    public let results: [Clubs]
+    
+    init(results: [Clubs]) {
+        self.results = results
+    }
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: RootCodingKeys.self)
+        self.results = try container.decode([Clubs].self, forKey: .response)
+    }
+    
+}
