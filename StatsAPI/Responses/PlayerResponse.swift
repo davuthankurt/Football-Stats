188f180376d7b1459fe4140179d1d502fe7df999
davut <davuthankurt@gmail.com>
2024-11-15 16:15:14 +0300

    conform player data request

--- StatsAPI/Responses/PlayerResponse.swift
+++ StatsAPI/Responses/PlayerResponse.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+public struct PlayerResponse: Codable {
+    private enum RootCodingKeys: String, CodingKey {
+        case response
+    }
+    
+    public let response: [PlayerInfo]
+    
+    init(response: [PlayerInfo]) {
+        self.response = response
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: RootCodingKeys.self)
+        self.response = try container.decode([PlayerInfo].self, forKey: .response)
+    }
+}
