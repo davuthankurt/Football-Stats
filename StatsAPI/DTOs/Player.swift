d03b1f3ef1675c960dae6cf3101e7838ea0c445e
davut <davuthankurt@gmail.com>
2024-11-15 16:18:06 +0300

    player data structure

--- StatsAPI/DTOs/Player.swift
+++ StatsAPI/DTOs/Player.swift
@@ -25,24 +25,13 @@
 //        "firstname": "Neymar",
 //        "lastname": "da Silva Santos Júnior",
 //        "age": 29,
-//        "birth": {
-//          "date": "1992-02-05",
-//          "place": "Mogi das Cruzes",
-//          "country": "Brazil"
-//        },
 //        "nationality": "Brazil",
 //        "height": "175 cm",
 //        "weight": "68 kg",
-//        "injured": false,
 //        "photo": "https://media.api-sports.io/football/players/276.png"
 //      },
 //      "statistics": [
 //        {
-//          "team": {
-//            "id": 85,
-//            "name": "Paris Saint Germain",
-//            "logo": "https://media.api-sports.io/football/teams/85.png"
-//          },
 //          "league": {
 //            "id": 61,
 //            "name": "Ligue 1",
@@ -53,22 +42,14 @@
 //          },
 //          "games": {
 //            "appearences": 13,
-//            "lineups": 10,
-//            "minutes": 975,
-//            "number": null,
-//            "position": "Attacker",
-//            "rating": "7.307692",
-//            "captain": false
+//            "lineups": 10
 //          },
 //          "goals": {
 //            "total": 6,
-//            "conceded": 0,
-//            "assists": 3,
-//            "saves": null
+//            "assists": 3
 //          },
 //          "cards": {
-//            "yellow": 5,
-//            "yellowred": 1,
+//            "yellow": 5
 //            "red": 1
 //          },
 //        },
@@ -78,5 +59,51 @@
 //}
 
 import Foundation
+
+public struct PlayerInfo: Codable {
+    public let player: Player
+    public let statistics: [Statistics]
+}
+
+public struct Player: Codable {
+    public let id: Int
+    public let name: String
+    public let age: Int
+    public let nationality: String
+    public let height: String?
+    public let weight: String?
+    public let photo: String
+}
+
+public struct Statistics: Codable {
+    public let team: Team
+    public let league: League
+    public let games: Games
+    public let goals: Goals
+    public let cards: Cards
+    
+    public struct League: Codable {
+        public let country: String
+        public let flag: String
+        public let id: Int
+        public let logo: String
+        public let name: String
+    }
+}
+
+public struct Games: Codable {
+    public let appearences: Int?
+    public let lineups: Int?
+}
+
+public struct Goals: Codable {
+    public let total: Int
+    public let assists: Int
+}
+
+public struct Cards: Codable {
+    public let yellow: Int
+    public let red: Int
+}
 
 
