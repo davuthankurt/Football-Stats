9a4007fb54191fb9689d38bd8941c276a5fd41b0
davut <davuthankurt@gmail.com>
2024-11-15 16:06:39 +0300

    tab bar added

--- StatsApp/App/AppRouter.swift
+++ StatsApp/App/AppRouter.swift
@@ -9,9 +9,20 @@ import UIKit
 
 final class AppRouter {
     func start(window: UIWindow){
-        let viewController = LeaguesBuilder.make()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        window.rootViewController = navigationController
+        
+        let tabBarController = UITabBarController()
+        
+        let leagueViewController = LeaguesBuilder.make()
+        let leagueNavigationController = UINavigationController(rootViewController: leagueViewController)
+        leagueNavigationController.tabBarItem = UITabBarItem(title: "Leagues", image: UIImage(systemName: "list.dash"), tag: 0)
+        
+        let statsTrackerViewController = StatsTrackerBuilder.make()
+        let statsTrackerNavigationController = UINavigationController(rootViewController: statsTrackerViewController)
+        statsTrackerNavigationController.tabBarItem = UITabBarItem(title: "Stats Tracker", image: UIImage(systemName: "star"), tag: 1)
+        
+        tabBarController.viewControllers = [leagueNavigationController, statsTrackerNavigationController]
+        
+        window.rootViewController = tabBarController
         window.makeKeyAndVisible()
     }
 }
