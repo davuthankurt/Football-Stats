a26fd299c8da79fe6e5c43beb40b44bc9b6b88c2
davut <davuthankurt@gmail.com>
2024-11-16 17:57:23 +0300

    st cell presentation logic

--- StatsApp/StatsTracker/StatsTrackerCell.swift
+++ StatsApp/StatsTracker/StatsTrackerCell.swift
@@ -5,4 +5,108 @@
 //  Created by Davuthan Kurt on 7.11.2024.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+//Temporary Cell Code
+class StatsTrackerCell: UICollectionViewCell {
+    
+    var cellTitle = UILabel()
+    var tableView = UITableView()
+    
+    var viewModel: StatsTrackerCellViewModel? {
+        didSet {
+            viewModel?.delegate = self
+            tableView.reloadData()
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureContents()
+        setConstraints()
+        viewModel?.getPlayers()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureContents() {
+        configureTitle()
+        configureTableView()
+    }
+    
+    private func configureTitle() {
+        contentView.addSubview(cellTitle)
+        
+        cellTitle.textAlignment = .center
+        cellTitle.font = UIFont(name: "AvenirNext-BoldItalic", size: 16)
+        cellTitle.textColor = UIColor(red: 36/255, green: 54/255, blue: 66/255, alpha: 1)
+        cellTitle.backgroundColor = UIColor(red: 98/255, green: 149/255, blue: 132/255, alpha: 1)
+    }
+    
+    private func configureTableView(){
+        contentView.addSubview(tableView)
+        
+        tableView.register(StatsTrackerPlayerCell.self, forCellReuseIdentifier: "Cell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.isScrollEnabled = false
+    }
+    
+    private func setConstraints(){
+        
+        cellTitle.snp.makeConstraints { make in
+            make.height.equalTo(44)
+            make.top.leading.trailing.equalTo(contentView)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(cellTitle.snp.bottom)
+            make.bottom.leading.trailing.equalTo(contentView)
+        }
+    }
+
+    func configureCell(with header: String ) {
+        cellTitle.text = header
+    }
+    
+    func setViewModel(viewModel: StatsTrackerCellViewModel) {
+        self.viewModel = viewModel
+    }
+}
+
+extension StatsTrackerCell: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let viewModel = viewModel else { return 0 }
+        return viewModel.numberOfRowsInSection
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? StatsTrackerPlayerCell else { return UITableViewCell() }
+        guard let viewModel = viewModel else { return cell }
+        
+        cell.configureCell(with: viewModel.cellForRowAt(index: indexPath), stats: viewModel.goalOrAssist, index: indexPath)
+        
+        return cell
+    }
+}
+
+
+extension StatsTrackerCell: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
+
+extension StatsTrackerCell: STCellViewModelDelegate {
+    func handleViewModelOutput(_ output: STCellViewModelOutput) {
+        switch output {
+        case .showPlayers:
+            tableView.reloadData()
+        }
+    }
+}
--- StatsApp/StatsTracker/StatsTrackerPlayerCell.swift
+++ StatsApp/StatsTracker/StatsTrackerPlayerCell.swift
@@ -5,4 +5,77 @@
 //  Created by Davuthan Kurt on 9.11.2024.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import Kingfisher
+
+class StatsTrackerPlayerCell: UITableViewCell {
+    
+    var playerRank = UILabel()
+    var playerImage = UIImageView()
+    var playerName = UILabel()
+    var playerScore = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        setupView()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView(){
+        contentView.addSubview(playerRank)
+        contentView.addSubview(playerImage)
+        contentView.addSubview(playerName)
+        contentView.addSubview(playerScore)
+        
+        playerRank.textAlignment = .left
+        playerName.textAlignment = .left
+        playerScore.textAlignment = .center
+        playerImage.contentMode = .scaleAspectFit
+    }
+    
+    func setupConstraints() {
+        
+        playerRank.snp.makeConstraints { make in
+            make.leading.equalTo(contentView.snp.leadingMargin)
+            make.width.equalTo(15)
+            make.centerY.equalToSuperview()
+        }
+        
+        playerImage.snp.makeConstraints { make in
+            make.leading.equalTo(playerRank.snp.trailing).offset(4)
+            make.width.height.equalTo(40)
+            make.centerY.equalToSuperview()
+        }
+        
+        playerName.snp.makeConstraints { make in
+            make.leading.equalTo(playerImage.snp.trailing).offset(4)
+            make.centerY.equalToSuperview()
+        }
+        
+        playerScore.snp.makeConstraints { make in
+            make.trailing.equalTo(contentView.snp.trailingMargin)
+            make.centerY.equalTo(contentView.snp.centerY)
+        }
+    }
+    
+    func configureCell(with footballer: PlayerPresentation, stats: Bool, index: IndexPath) {
+        guard let playerStat = footballer.statistics.first else { return }
+        playerName.text = footballer.player.name
+        if stats == true {
+            playerScore.text = "\(playerStat.goals.total)"
+        } else {
+            playerScore.text = "\(playerStat.goals.assists)"
+        }
+        guard let footballerImage = footballer.player.photo else { return }
+        let url = URL(string: footballerImage)
+        playerImage.kf.setImage(with: url)
+        
+        playerRank.text = "\(index.row + 1)."
+    }
+}
