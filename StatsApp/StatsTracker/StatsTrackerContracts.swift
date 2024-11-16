934f0404aef768ed2c93e59fc7da4fdd148c9237
davut <davuthankurt@gmail.com>
2024-11-16 17:47:36 +0300

    st view model protocols

--- StatsApp/StatsTracker/StatsTrackerContracts.swift
+++ StatsApp/StatsTracker/StatsTrackerContracts.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+
+protocol StatsTrackerViewModelProtocol {
+    var delegate: StatsTrackerViewModelDelegate? { get set }
+    var leaguesGoalScorers: [[PlayerPresentation]] { get }
+    var leaguesAssisters: [[PlayerPresentation]] { get }
+    func loadTopScorers() async
+    func loadTopAsisters() async
+    func numberOfSections() -> Int
+    func numberOfItemsInSection(section: Int) -> Int
+    func cellForItemAt(index: IndexPath) -> String
+    func viewForSupplementaryElementOfKind(index: IndexPath) -> String
+    func injectCellViewModel(index: IndexPath) -> StatsTrackerCellViewModel
+}
+
+enum StatsTrackerViewModelOutput {
+    case showTopScorers
+    case showTopAssisters
+}
+
+protocol StatsTrackerViewModelDelegate {
+    func handleViewModelOutput(_ output: StatsTrackerViewModelOutput)
+}
