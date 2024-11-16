a542ff5980e1d6e8970289a7cec4b1c682fdfd2b
davut <davuthankurt@gmail.com>
2024-11-16 17:46:29 +0300

    st collection view header

--- StatsApp/StatsTracker/LeagueHeader.swift
+++ StatsApp/StatsTracker/LeagueHeader.swift
@@ -5,4 +5,44 @@
 //  Created by Davuthan Kurt on 8.11.2024.
 //
 
-import Foundation
+//    UIColor(red: 36/255, green: 54/255, blue: 66/255, alpha: 1).cgColor,
+//    UIColor(red: 56/255, green: 116/255, blue: 120/255, alpha: 1).cgColor,
+//    UIColor(red: 98/255, green: 149/255, blue: 132/255, alpha: 1).cgColor,
+//    UIColor(red: 226/255, green: 241/255, blue: 231/255, alpha: 1).cgColor
+
+import UIKit
+
+class LeagueHeader: UICollectionReusableView {
+    
+    static let reuseIdentifier = "CustomHeaderView"
+    private let titleLabel = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureTitle()
+        setTitleConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureTitle() {
+        addSubview(titleLabel)
+        titleLabel.font = UIFont(name: "AvenirNext-BoldItalic", size: 20)
+        titleLabel.textColor =  UIColor(red: 36/255, green: 54/255, blue: 66/255, alpha: 1)
+    }
+    
+    func setTitleConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    func configure(with title: String) {
+        titleLabel.text = title
+    }
+}
