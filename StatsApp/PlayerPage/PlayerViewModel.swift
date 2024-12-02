22ed8a432834092f9398ad35bcd9fd03a99187fb
davut <davuthankurt@gmail.com>
2024-12-02 20:06:25 +0300

    player view model created

--- StatsApp/PlayerPage/PlayerViewModel.swift
+++ StatsApp/PlayerPage/PlayerViewModel.swift
@@ -9,10 +9,15 @@ import Foundation
 import StatsAPI
 
 final class PlayerViewModel: PlayerViewModelProtocol {
+    
+    
     weak var delegete: PlayerViewModelDelegate?
     private let service: AppService
     let playerId: Int
-    
+    var presentation: PlayerPresentation?
+    var playerProfile: [(title: String, value: String)] = []
+    var playerStat: [(title: String, value: String)] = []
+        
     init(service: AppService, playerId: Int) {
         self.service = service
         self.playerId = playerId
@@ -22,24 +27,117 @@ final class PlayerViewModel: PlayerViewModelProtocol {
 extension PlayerViewModel {
     
     func loadPlayer() {
-        
+        notify(.updateTitle("DAKY Stats"))
         service.fetchData(from: .player(id: playerId), responseType: PlayerResponse.self) { [weak self] result in
             guard let self else { return }
-            
             switch result {
             case .success(let response):
                 guard let footballer = response.response.first else { return }
-                let presentation = PlayerPresentation(footballer: footballer)
+                presentation = PlayerPresentation(footballer: footballer)
+//                playerProfile = presentation?.player
+                setProfileData()
                 notify(.showPlayerPage)
-                print(presentation.player.name)
-                
-                
-                
             case .failure(let error):
                 print(error.localizedDescription)
             }
-        
         }
+    }
+    
+    func setProfileData() {
+        let height = presentation?.player.height ?? "-"
+        let weight = presentation?.player.weight ?? "-"
+        let nationality = presentation?.player.nationality ?? "-"
+        let age = presentation?.player.age.map{String($0)} ?? "-"
+        playerProfile = [
+            ("Age:", age),
+            ("Height:", height),
+            ("Weight:", weight),
+            ("Nationality:", nationality)
+        ]
+    }
+
+    func numberOfItemsInSection(section: Int) -> Int {
+        if section == 0 {
+            return 4
+        } else {
+            return 8
+        }
+    }
+    
+    func cellForItemAt(index: IndexPath) -> PlayerOrStatistics {
+         
+        if index.item == 0 {
+            return .player(data: presentation!.player)
+        } else {
+            return .stats(data: presentation!.statistics[index.item-1])
+        }
+    }
+    
+    func numberOfRowsInComponent() -> Int {
+        guard let presentation = presentation else { return 0 }
+        var leagueNames = [String]()
+        return 1 + presentation.statistics.count
+    }
+    
+    func titleForRow(row: Int) -> String {
+        guard let presentation = presentation else { return "" }
+        if row == 0 {
+            return "Profile"
+        } else {
+            guard let name = presentation.statistics[row-1].league.name else { return ""}
+            setStatData(row: row)
+            return name
+        }
+    }
+    
+    func setStatData(row: Int){
+        guard let presentation = presentation else { return }
+        let appearances = presentation.statistics[row-1].games.appearences.map { String($0) } ?? "-"
+        let lineups = presentation.statistics[row-1].games.lineups.map { String($0) } ?? "-"
+        let minutes = presentation.statistics[row-1].games.minutes.map { String($0) } ?? "-"
+        var rating = presentation.statistics[row-1].games.rating ?? "-"
+        let totalGoals = presentation.statistics[row-1].goals.total.map { String($0) } ?? "-"
+        let assists = presentation.statistics[row-1].goals.assists.map { String($0) } ?? "-"
+        let yellowCards = presentation.statistics[row-1].cards.yellow.map { String($0) } ?? "-"
+        let redCards = presentation.statistics[row-1].cards.red.map { String($0) } ?? "-"
+        rating = String(rating.prefix(4))
+        
+        playerStat = [
+            ("Appearances:", appearances),
+            ("Lineups:", lineups),
+            ("Minutes:", minutes),
+            ("Rating:", rating),
+            ("Goals:", totalGoals),
+            ("Assists:", assists),
+            ("Yellow Cards:", yellowCards),
+            ("Red Cards:",redCards)
+        ]
+    }
+    
+    func getSegmentTitles() -> [String] {
+        guard let presentation = presentation else { return ["-"] }
+        var leagueNames = [String]()
+        for i in 0..<presentation.statistics.count{
+            if let leagueName = presentation.statistics[i].league.name {
+                leagueNames.append(leagueName)
+            }
+        }
+        return ["Profile"] + leagueNames
+    }
+    
+    func setPlayerImage() -> String {
+        guard let playerPresentation = presentation, let playerPhoto = playerPresentation.player.photo else { return "-" }
+        return playerPhoto
+    }
+    
+    func setPlayerName() ->String {
+        guard let playerPresentation = presentation, 
+              let playerFirstName = playerPresentation.player.firstname,
+              let playerLastname = playerPresentation.player.lastname else { 
+            guard let playerPresentation = presentation,
+                  let playerName = playerPresentation.player.name else { return "-" }
+            return playerName }
+        return playerFirstName + " " + playerLastname
     }
     
     func notify(_ output: PlayerViewModelOutput) {
