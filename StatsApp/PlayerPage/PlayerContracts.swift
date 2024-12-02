ce142afe7fb6194304243add98c0d32bac0832d0
davut <davuthankurt@gmail.com>
2024-12-02 20:04:23 +0300

    player page protocols

--- StatsApp/PlayerPage/PlayerContracts.swift
+++ StatsApp/PlayerPage/PlayerContracts.swift
@@ -6,14 +6,29 @@
 //
 
 import Foundation
+import StatsAPI
 
 protocol PlayerViewModelProtocol {
     var delegete: PlayerViewModelDelegate? { get set }
+    var playerProfile: [(title: String, value: String)] { get }
+    var playerStat: [(title: String, value: String)] { get }
+    func setProfileData()
     func loadPlayer()
+    func numberOfRowsInComponent() -> Int 
+    func titleForRow(row: Int) -> String
+    func setPlayerImage() -> String
+    func setPlayerName() -> String
+    func cellForItemAt(index: IndexPath) -> PlayerOrStatistics
+}
+
+enum PlayerOrStatistics {
+    case player(data: Player)
+    case stats(data: Statistics)
 }
 
 enum PlayerViewModelOutput {
     case showPlayerPage
+    case updateTitle(String)
 }
 
 protocol PlayerViewModelDelegate: AnyObject {
